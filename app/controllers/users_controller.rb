bf64099cd6591f3ea7a1a2d7e3e5a7afeabb95ae
Anand <21anand14@gmail.com>
2021-08-26 04:48:53 -0700

    Edit functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     def new
         @user=User.new
     end
+
     def create
         #byebug
         @user=User.new(user_params)
@@ -10,6 +11,20 @@ class UsersController < ApplicationController
             redirect_to articles_path
         else
             render 'new'
+        end
+    end
+
+    def edit
+        @user=User.find(params[:id])
+    end
+
+    def update
+        @user=User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice]="Your account information was successfully updated"
+            redirect_to articles_path
+        else
+            render 'edit'
         end
     end
 
