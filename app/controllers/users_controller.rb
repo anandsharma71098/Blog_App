4dfec7e50ce78799cd24bba3dbbf6621d2c1e3ec
Anand <21anand14@gmail.com>
2021-08-26 22:28:28 -0700

    Partial complete show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+    def show
+        @user=User.find(params[:id])
+    end
+
     def new
         @user=User.new
     end
@@ -27,6 +32,7 @@ class UsersController < ApplicationController
             render 'edit'
         end
     end
+
 
     private
     def user_params
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def gravatar_for(user, options = { size: 80})
+        email_address = user.email.downcase
+        hash= Digest::MD5.hexdigest(email_address)
+        size = options[:size]
+        gavatar_url="https://www.gravatar.com/avatar/#{hash}?=#{size}"
+        image_tag(gavatar_url, alt: user.username)
+    end
 end
