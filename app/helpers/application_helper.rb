dfb0f83614640ac9c80a4d72f0a26dbcfcd4d80a
Anand <21anand14@gmail.com>
2021-08-26 23:53:18 -0700

    Show action added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
         email_address = user.email.downcase
         hash= Digest::MD5.hexdigest(email_address)
         size = options[:size]
-        gavatar_url="https://www.gravatar.com/avatar/#{hash}?=#{size}"
-        image_tag(gavatar_url, alt: user.username)
+        gavatar_url="https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        image_tag(gavatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
     end
 end
