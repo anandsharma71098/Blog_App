6aa49e6c4ac68487246c5560e7b252ea7df6b757
Anand <21anand14@gmail.com>
2021-08-30 04:31:43 -0700

    Admin Functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
 
     def destroy
         @user.destroy
-        session[:user_id]=nil
+        session[:user_id]=nil if @user==current_user
         flash[:notice]="Account and all associated articles successfully deleted"
         redirect_to articles_path
     end
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-        if current_user!=@user
+        if current_user!=@user && !current_user.admin?
             flash[:alert]="You can only edit your own account"
             redirect_to @user
         end
